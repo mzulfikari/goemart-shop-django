7dcde07cf1d02a461e84f897181d5f5c514b2bb0
mzulfikari <mzulfikari1382@gmail.com>
2025-10-29 01:09:01 +0330

    end list product

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -9,3 +9,13 @@ def show_latest_product():
     latest_product = ProductModel.objects.filter(
         status=ProductStatusType.publish.value).order_by("-created_date")[:8]
     return {"latest_products":latest_product}
+
+
+@register.inclusion_tag('includes/similar-product.html')
+def show_similar_product(product):
+    product_categories = product.category.all()
+    similar_product = ProductModel.objects.filter(
+        status=ProductStatusType.publish.value,category__in=product_categories).order_by("-created_date")[:4]
+    return {"similar_products":similar_product}
+
+
