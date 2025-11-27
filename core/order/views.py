647d2fc75e8da870325255ddb0abf2842954fa24
mzulfikari <mzulfikari1382@gmail.com>
2025-11-27 13:12:54 +0330

    context total_price the tmplates checkout

--- core/order/views.py
+++ core/order/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import( 
+    FormView,
+    TemplateView,
+)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from dashboard.permissions import HasCustomerAccessPermission
+from order.models import UserAddressModel
+from cart.models import CartModel
+from .forms import CheckOutForm
 
-
-class OrderCheckOutView(LoginRequiredMixin,HasCustomerAccessPermission,TemplateView):
+class OrderCheckOutView(LoginRequiredMixin,HasCustomerAccessPermission,FormView):
     
     template_name = 'order/checkout.html'
+    form_class = CheckOutForm
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart = CartModel.objects.get(user=self.request.user)
+        context['addresses'] = UserAddressModel.objects.filter(user=self.request.user)
+        total_price = cart.calculate_total_price()
+        context ['total_price'] = total_price
+        context["total_max"] = int(total_price * 0.09)
+        return context
